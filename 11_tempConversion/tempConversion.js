6f02e46779e769141df108ad38d62546041e1e59
KurtGolisao <kjgolisao@gmail.com>
2026-08-01 23:50:45 +0800

    Finished activity

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(inputNumber) {
+  var outputUnrounded = (inputNumber - 32) * (5/9);
+  return Math.round(outputUnrounded * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(inputNumber) {
+  var outputUnrounded = (inputNumber * (9/5)) + 32;
+  return Math.round(outputUnrounded * 10) / 10;
 };
+
+console.log(convertToCelsius(32));
+console.log(convertToFahrenheit(0));
 
 // Do not edit below this line
 module.exports = {
