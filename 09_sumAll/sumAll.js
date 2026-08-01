20a8a7f807b3d64e15dd8a64f0891fad01635eb5
KurtGolisao <kjgolisao@gmail.com>
2026-08-01 22:51:10 +0800

    Finished activity

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, secondNum) {
+    let output = [];
+    output.push(firstNum);
+    while (firstNum != secondNum) {
+        if (firstNum < 0 || secondNum < 0) {
+            return "ERROR";
+        } else if (firstNum % 1 != 0 || secondNum % 1 != 0) {
+            return "ERROR";
+        } else if (typeof firstNum != "number" || typeof secondNum != "number") {
+            return "ERROR";
+        }else if (firstNum > secondNum) {
+            firstNum--;
+            output.push(firstNum);
+        } else {
+            firstNum++;
+            output.push(firstNum);
+        }
+    }
+    return output.reduce((toAdd, currentValue) => toAdd + currentValue, 0);
 };
+
+console.log(sumAll(123, 1));
 
 // Do not edit below this line
 module.exports = sumAll;
