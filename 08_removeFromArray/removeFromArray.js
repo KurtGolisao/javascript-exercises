7e02e324489498d02c5151d0b3796e29903340cd
KurtGolisao <kjgolisao@gmail.com>
2026-08-01 16:41:10 +0800

    Finished activity

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...toRemove) {
+    output = (array.filter((number) => !toRemove.includes(number)));
+    return output;
 };
 
 // Do not edit below this line
