7bf3991e4ce519ad5015fc366da8a199d777a1cb
KurtGolisao <kjgolisao@gmail.com>
2026-08-01 23:06:12 +0800

    Finished Activity

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(yearInput) {
+    if (yearInput % 4 === 0 && yearInput % 100 != 0) {
+        return true;
+    } else if (yearInput % 400 === 0 && yearInput % 100 === 0) {
+        console.log(yearInput + "is valid.")
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
